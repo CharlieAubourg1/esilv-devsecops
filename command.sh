30904f5ded19bc39be78704721dda303549ed673
Giougt <154607600+Giougt@users.noreply.github.com>
2025-11-23 17:30:38 +0100

    config SQL OK

--- command.sh
+++ command.sh
@@ -9,3 +9,7 @@ kubectl get pods
 kubectl get pvc
 kubectl get svc
 kubectl exec -it mysql-pod -- mysql -u root -p
+kubectl cp db/init.sql mysql-pod:/init.sql
+kubectl exec -it mysql-pod -- bash
+mysql -u root -p < /init.sql
+kubectl exec -it mysql-pod -- mysql -u root -p
