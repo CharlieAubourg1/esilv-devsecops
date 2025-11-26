265575a93bd12bb38c7fc998db6f51f84dbde609
Giougt <154607600+Giougt@users.noreply.github.com>
2025-11-26 02:32:53 +0100

    error db connection failed in node's container

--- command.sh
+++ command.sh
@@ -28,6 +28,32 @@ kubectl get pods
 kubectl get pods
 kubectl get deployments
 kubectl scale deployment bakery-api --replicas=0
-kubectl scale deployment bakery-api --replicas=1 # restart
+kubectl scale deployment bakery-api --replicas=1 # restart frontend
 kubectl delete pod mysql-pod
 kubectl get pods
+docker build -t my-mysql .
+docker images
+kubectl apply -f deployment-db.yaml #recreate pod mysql
+kubectl get pods
+cd 
+minikube image load my-mysql:latest #error
+kubectl delete pod mysql-pod #clean error
+kubectl get pods # clean error 
+#create new container mysql with pod 
+docker build -t my-mysql . 
+docker volume create mysql-data #data are now save here
+docker stop mysql-container
+docker rm mysql-container #wrong idea
+kubectl get pods
+kubectl get pods #searche something 
+# command to sucess running after configuration
+kubectl apply -f deployment-db.yaml    
+# try connection pod 
+kubectl exec -it bakery-api-6f746b586f-ts9bs -- sh #nothing 
+#faile mysql not found 
+# apply change 
+kubectl apply -f app/deployment-app.yaml
+# destroy again pod 
+kubectl delete pod mysql-pod
+# next step
+kubectl apply -f deployment-db.yaml #recreate pod mysql
