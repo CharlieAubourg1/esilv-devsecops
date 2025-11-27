4966dd884f804b2a7adba2e99ede2a3737f87917
CharlieAubourg1 <charlie.aubourg@gmail.com>
2025-11-27 23:48:44 +0100

    [webapp] final fixes

--- app/src/server.js
+++ app/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://127.0.0.1:3000', 'http://192.168.49.2:30001'], 
+    origin: 'http://127.0.0.1:3000', 
     credentials: true
 }));
 
